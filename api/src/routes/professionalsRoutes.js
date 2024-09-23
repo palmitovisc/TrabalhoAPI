3d338ebbfb0d7ee357945408e31767035c1fee89
Ps-lia <116975308+Ps-lia@users.noreply.github.com>
2024-09-23 10:56:17 -0300

    Add files via upload

---
+++ api/src/routes/professionalsRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+let profDB = require('../DB/professionals.json');
+
+function loadProfissionais() {
+    try {
+       
+        profDB = JSON.parse(fs.readFileSync('./src/DB/professionals.json', 'utf8'));
+        console.log('Dados carregados diretamente do arquivo.');
+    } catch (err){
+        console.error('Erro ao ler o arquivo:', err);
+    }
+}
+
+
+function escreve(dadosEcrita) {
+    try {
+        fs.writeFileSync('./src/DB/professionals.json', JSON.stringify(dadosEcrita, null, 2), 'utf8');
+        console.log('Dados escritos com sucesso');
+    } catch (err) {
+        console.error('Não foi possível escrever no arquivo:', err);
+    }
+}
+
+router.get('/', (req, res) => {
+    loadProfissionais();
+    try {
+        res.json(profDB);
+    } catch (err) {
+        console.error("Erro ao enviar dados.");
+    }
+});
+
+
+
+router.post('/adicionar', (req,res) =>{
+    loadProfissionais();
+    const prof = req.body;
+    const id = uuidv4();
+    const profComId = {
+        id: id,
+        ...prof
+    };
+    profDB.push(profComId);
+    escreve(profDB);
+    res.status(201).send('novo profissional adicionado');
+    
+});
+
+
+router.put('/:id', (req,res) =>{
+    loadProfissionais();
+    const id = req.params.id;
+    const profNovo = req.body;
+    const profIndex = profDB.findIndex(prof => prof.id === id);
+
+    profDB[profIndex] = { ...profDB[profIndex], ...profNovo };
+    escreve(profDB);
+    res.json(profDB[profIndex]);
+});
+
+
+router.delete('/:id', (req,res) =>{
+    loadProfissionais();
+    const id = req.params.id;
+    const profIndex = profDB.findIndex(prof => prof.id === id);
+    profDB.splice(profIndex, 1);
+    escreve(profDB);
+    res.status(200).send('dados removidos com sucesso');
+});
+module.exports = router;
