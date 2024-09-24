efb54f53f871d3abf2b53c3a4f405f5677bd393c
Rafael Palmito <122941204+palmitovisc@users.noreply.github.com>
2024-09-24 09:39:07 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const agenda = require('./appointRoutes');
 const prof = require('./professionalsRoutes');
+const user = require('./userRoutes');
 const { v4: uuidv4 } = require('uuid');
 
 
 router.use(express.json());
 router.use('/appoint', agenda );
 router.use('/prof', prof);
+router.use('/user', user);
 
 module.exports = router;
