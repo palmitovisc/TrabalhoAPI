d32e913d82620b8d758b4ddaa0e4dcce0ad88465
Rafael Palmito <122941204+palmitovisc@users.noreply.github.com>
2024-09-26 11:54:46 -0300

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,45 @@
-const express = require('express')
-const app = express()
-const routes = require('./routes')
-const { v4: uuidv4 } = require('uuid')
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const { v4: uuidv4 } = require('uuid');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
-app.use(function(req, res, next){ //
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-   }); //
+const cors = require("cors");
+app.use(cors());
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API Trabalho",
+			version: "1.0.0",
+			description: `API do trabalho de desenvolvimento de aplicações.  
+            
+            ### Trabalho 1    
+            Disciplina: DAII 2024.02 Turma 01  
+            Equipe: Rafael Martignago Palmito, Gabriel Flor, Élita Pereira, Bruno Souza, João Victor Lourenço.
+			`,
+      license: {
+        name: 'Licenciado para DAII',
+      },
+      contact: {
+        name: 'Rafael Martignago Palmito'
+      },
+		},
+		servers: [
+			{
+				url: "http://localhost:3000/",
+        description: 'Development server',
+			},
+		],
+	},
+	apis: ["./src/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
 
 app.use('/', routes)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.listen(3000, function () { 
     console.log('Aplicação executando na porta http://127.0.0.1:3000/'); }); 
