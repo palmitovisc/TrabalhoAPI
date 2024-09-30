f1506f41c633ea0a85f5b61b523c0177fc1928aa
Ps-lia <116975308+Ps-lia@users.noreply.github.com>
2024-09-29 23:25:10 -0300

    professionalsRoutes.js

--- api/src/routes/professionalsRoutes.js
+++ api/src/routes/professionalsRoutes.js
@@ -1,93 +1,210 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
-let profDB = require("../DB/professionals.json");
+let profDB = require('../DB/professionals.json');
 
 function loadProfissionais() {
-  try {
-    profDB = JSON.parse(fs.readFileSync("./src/DB/professionals.json", "utf8"));
-    console.log("Dados carregados diretamente do arquivo.");
-  } catch (err) {
-    console.error("Erro ao ler o arquivo:", err);
-  }
+    try {
+        profDB = JSON.parse(fs.readFileSync('./src/DB/professionals.json', 'utf8'));
+        console.log('Dados carregados diretamente do arquivo.');
+    } catch (err) {
+        console.error('Erro ao ler o arquivo:', err);
+    }
 }
 
 function escreve(dadosEcrita) {
-  try {
-    fs.writeFileSync(
-      "./src/DB/professionals.json",
-      JSON.stringify(dadosEcrita, null, 2),
-      "utf8"
-    );
-    console.log("Dados escritos com sucesso");
-  } catch (err) {
-    console.error("Não foi possível escrever no arquivo:", err);
-  }
+    try {
+        fs.writeFileSync('./src/DB/professionals.json', JSON.stringify(dadosEcrita, null, 2), 'utf8');
+        console.log('Dados escritos com sucesso');
+    } catch (err) {
+        console.error('Não foi possível escrever no arquivo:', err);
+    }
 }
 
-router.get("/", (req, res) => {
-  loadProfissionais();
-  try {
-    res.json(profDB);
-  } catch (err) {
-    console.error("Erro ao enviar dados.");
-  }
+// Swagger definition
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Profissionais',
+            version: '1.0.0',
+            description: 'API para controle de profissionais',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['professionalsRoutes.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Suas rotas e documentação Swagger
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Professional:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - role
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: ID gerado automaticamente para o profissional
+ *         name:
+ *           type: string
+ *           description: Nome do profissional
+ *         role:
+ *           type: string
+ *           description: Função do profissional
+ *       example:
+ *         id: "7a6cc1282c5f6ec0235acd2bfa780145aa2a67fd"
+ *         name: "João Silva"
+ *         role: "Desenvolvedor"
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Professionals
+ *   description: API de controle de profissionais
+ */
+
+/**
+ * @swagger
+ * /professionals:
+ *   get:
+ *     summary: Retorna a lista de todos os profissionais
+ *     tags: [Professionals]
+ *     responses:
+ *       200:
+ *         description: A lista de profissionais
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Professional'
+ */
+router.get('/', (req, res) => {
+    loadProfissionais();
+    try {
+        res.json(profDB);
+    } catch (err) {
+        console.error("Erro ao enviar dados.");
+    }
 });
 
-router.get("/:id", (req, res) => {
-  loadProfissionais();
-  const id = req.params.id;
-  const profissional = profDB.find((prof) => prof.id === id);
-
-  if (profissional) {
-    res.json(profissional);
-  } else {
-    res.status(404).send("Profissional não encontrado");
-  }
-});
-
-router.post("/adicionar", (req, res) => {
-  loadProfissionais();
-  const prof = req.body;
-  const id = uuidv4();
-  const profComId = {
-    id: id,
-    ...prof,
-  };
-  profDB.push(profComId);
-  escreve(profDB);
-  res.status(201).send("Novo profissional adicionado");
-});
-
-router.put("/:id", (req, res) => {
-  loadProfissionais();
-  const id = req.params.id;
-  const profNovo = req.body;
-  const profIndex = profDB.findIndex((prof) => prof.id === id);
-
-  if (profIndex !== -1) {
-    profDB[profIndex] = { ...profDB[profIndex], ...profNovo };
+/**
+ * @swagger
+ * /professionals/adicionar:
+ *   post:
+ *     summary: Adiciona um novo profissional
+ *     tags: [Professionals]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Professional'
+ *     responses:
+ *       201:
+ *         description: Profissional adicionado com sucesso
+ */
+router.post('/adicionar', (req, res) => {
+    loadProfissionais();
+    const profissional = req.body;
+    const id = uuidv4();
+    const profissionalComId = {
+        id: id,
+        ...profissional
+    };
+    profDB.push(profissionalComId);
     escreve(profDB);
-    res.json(profDB[profIndex]);
-  } else {
-    res.status(404).send("Profissional não encontrado");
-  }
+    res.status(201).send('novo profissional adicionado');
 });
 
-router.delete("/:id", (req, res) => {
-  loadProfissionais();
-  const id = req.params.id;
-  const profIndex = profDB.findIndex((prof) => prof.id === id);
+/**
+ * @swagger
+ * /professionals/{id}:
+ *   put:
+ *     summary: Atualiza um profissional pelo ID
+ *     tags: [Professionals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do profissional a ser atualizado
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Professional'
+ *     responses:
+ *       200:
+ *         description: Profissional atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professional'
+ */
+router.put('/:id', (req, res) => {
+    loadProfissionais();
+    const id = req.params.id;
+    const profissionalNovo = req.body;
+    const profissionalIndex = profDB.findIndex(profissional => profissional.id === id);
 
-  if (profIndex !== -1) {
-    profDB.splice(profIndex, 1);
-    escreve(profDB);
-    res.status(200).send("Dados removidos com sucesso");
-  } else {
-    res.status(404).send("Profissional não encontrado");
-  }
+    if (profissionalIndex !== -1) {
+        profDB[profissionalIndex] = { ...profDB[profissionalIndex], ...profissionalNovo };
+        escreve(profDB);
+        res.json(profDB[profissionalIndex]);
+    } else {
+        res.status(404).send("Profissional não encontrado");
+    }
+});
+
+/**
+ * @swagger
+ * /professionals/{id}:
+ *   delete:
+ *     summary: Remove um profissional pelo ID
+ *     tags: [Professionals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do profissional a ser removido
+ *     responses:
+ *       200:
+ *         description: Profissional removido com sucesso
+ */
+router.delete('/:id', (req, res) => {
+    loadProfissionais();
+    const id = req.params.id;
+    const profissionalIndex = profDB.findIndex(profissional => profissional.id === id);
+
+    if (profissionalIndex !== -1) {
+        profDB.splice(profissionalIndex, 1);
+        escreve(profDB);
+        res.status(200).send('profissional removido com sucesso');
+    } else {
+        res.status(404).send("Profissional não encontrado");
+    }
 });
 
 module.exports = router;
