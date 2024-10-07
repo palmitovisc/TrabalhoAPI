f22c88566abd2eb859e26df6ce1e321a5dd45bb6
Rafael Palmito <122941204+palmitovisc@users.noreply.github.com>
2024-10-07 14:30:16 -0300

    Update studentsRoutes.js

--- api/src/routes/studentsRoutes.js
+++ api/src/routes/studentsRoutes.js
@@ -73,7 +73,7 @@ function writeStudents(data) {
  * @swagger
  * tags:
  *   name: Students
- *   description: API para gerenciamento de alunos | Desenvolvido por: **João Victor Borges Lourenço**"
+ *   description: "API para gerenciamento de alunos | Desenvolvido por: **João Victor Borges Lourenço**"
  */
 
 /**
